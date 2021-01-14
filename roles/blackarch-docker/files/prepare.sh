b903563544c079fbb4dc38e0fb39071a83d13bb0
galo1z <cherezov@protonmail.com>
2021-01-14 17:42:19 +0300

    Created black arch docker image, some work on kali docker image was done

---
+++ roles/blackarch-docker/files/prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#TODO: add download of stup.sh and delete internet connection check
+cp ../../tmux/files/tmux.conf configs/tmux.conf
+cp ../../zsh/files/zshrc configs/zshrc
+cp ../../nvim/files/hybrid_fixed.vim configs/hybrid_fixed.vim
+cp ../../nvim/files/init.vim configs/init.vim
