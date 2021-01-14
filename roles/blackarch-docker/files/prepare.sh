ab0dea4ba7cc26ad5d12f0359fa57bd736d6c85c
galo1z <cherezov@protonmail.com>
2021-01-14 19:06:43 +0300

    Fixed colorcheme

--- roles/blackarch-docker/files/prepare.sh
+++ roles/blackarch-docker/files/prepare.sh
@@ -2,5 +2,5 @@
 #TODO: add download of stup.sh and delete internet connection check
 cp ../../tmux/files/tmux.conf configs/tmux.conf
 cp ../../zsh/files/zshrc configs/zshrc
-cp ../../nvim/files/hybrid_fixed.vim configs/hybrid_fixed.vim
+cp ../../nvim/files/hybrid.vim configs/hybrid.vim
 cp ../../nvim/files/init.vim configs/init.vim
