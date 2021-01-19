2aaa8972cbf27a79643b3e8923ca481ac1a6acfa
galo1z <cherezov@protonmail.com>
2021-01-19 14:18:46 +0300

    Updated black arch role

---
+++ roles/blackarch-docker/files/msf_res.rb
@@ -0,0 +1,6 @@
+ <ruby>
+ run_single("set RHOSTS " + ENV['ip'])
+ run_single("set LHOST " + ENV['lip'])
+ run_single("set USERPASS_FILE " + ENV['pass_list'])
+ run_single("set PASS_FILE " + ENV['login_list'])
+ </ruby>
