4419a794f79cffabc9da8e06ed7e7ecd8efa8665
galo1z <cherezov@protonmail.com>
2021-07-10 14:35:45 +0300

    Blackarch update and small stuff

--- roles/blackarch-docker/files/configs/msf_res.rb
+++ roles/blackarch-docker/files/configs/msf_res.rb
@@ -1,6 +1,7 @@
  <ruby>
  run_single("set RHOSTS " + ENV['ip'])
  run_single("set LHOST " + ENV['lip'])
- run_single("set USERPASS_FILE " + ENV['pass_list'])
- run_single("set PASS_FILE " + ENV['login_list'])
+ run_single("set LPORT " + ENV['lport'])
+ run_single("set PASS_FILE " + ENV['pass_list'])
+ run_single("set USER_FILE " + ENV['login_lis'])
  </ruby>
